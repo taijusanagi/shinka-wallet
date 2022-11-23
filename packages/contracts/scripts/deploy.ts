a941be16b4b2bca95d1984ad65229ec3fd52a698
taijusanagi <taijusanagi@gmail.com>
2022-11-24 01:08:53 +0900

    feat: deploy to polygon zkEVM

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -9,12 +9,11 @@ import networkJsonFile from "../network.json";
 import { EntryPoint__factory, ShinkaWalletDeployer__factory, ShinkaWalletPaymaster__factory } from "../typechain-types";
 import { ChainId, isChainId } from "../types/ChainId";
 
+// polygon evm needs to specify gas limit manually
 async function main() {
   const chainId = String(network.config.chainId) as ChainId;
   console.log("chainId", chainId);
-  if (!isChainId(chainId)) {
-    throw new Error("chain id invalid");
-  }
+
   const signer = await ethers.provider.getSigner();
   const signerAddress = await signer.getAddress();
   console.log("signer", signerAddress);
@@ -23,22 +22,32 @@ async function main() {
   }
 
   const EntryPoint = new EntryPoint__factory(signer);
-  const entryPoint = await EntryPoint.deploy(PAYMASTER_STAKE, UNSTAKE_DELAY_SEC);
+  const entryPoint = await EntryPoint.deploy(PAYMASTER_STAKE, UNSTAKE_DELAY_SEC, {
+    gasLimit: chainId === "1402" ? 5120823 : undefined,
+  });
   await entryPoint.deployed();
 
   const Factory = new ShinkaWalletDeployer__factory(signer);
-  const factory = await Factory.deploy();
+  const factory = await Factory.deploy({
+    gasLimit: chainId === "1402" ? 2132333 : undefined,
+  });
   await factory.deployed();
 
   const Paymaster = new ShinkaWalletPaymaster__factory(signer);
-  const paymaster = await Paymaster.deploy(entryPoint.address, signerAddress);
+  const paymaster = await Paymaster.deploy(entryPoint.address, signerAddress, {
+    gasLimit: chainId === "1402" ? 1123775 : undefined,
+  });
   const deployments = {
     entryPoint: entryPoint.address,
     factory: factory.address,
     paymaster: paymaster.address,
   };
-  networkJsonFile[chainId].deployments = deployments;
-  fs.writeFileSync(path.join(__dirname, `../network.json`), JSON.stringify(networkJsonFile));
+
+  if (isChainId(chainId)) {
+    networkJsonFile[chainId].deployments = deployments;
+    fs.writeFileSync(path.join(__dirname, `../network.json`), JSON.stringify(networkJsonFile));
+  }
+
   console.log("deployements", deployments);
 }
 
