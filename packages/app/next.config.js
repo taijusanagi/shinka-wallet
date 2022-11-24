ae544b0b7acab91314a7ef8bf4b702aadb91ad74
taijusanagi <taijusanagi@gmail.com>
2022-11-24 19:53:22 +0700

    feat: pwa

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa");
+
 const nextConfig = {
+  pwa: {
+    dest: "public/",
+    disable: process.env.NODE_ENV === "development",
+  },
   // reactStrictMode: true, // this is removed for better demo
   swcMinify: true,
   experimental: {
@@ -7,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
