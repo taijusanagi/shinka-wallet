5beaa0b6de573028f47d993ca62a3f27cae87fb5
taijusanagi <taijusanagi@gmail.com>
2022-11-24 19:59:15 +0700

    chore: remove sw

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
-const nextConfig = {
-  pwa: {
-    dest: "public/",
-    disable: process.env.NODE_ENV === "development",
-  },
-  // reactStrictMode: true, // this is removed for better demo
+module.exports = withPWA({
   swcMinify: true,
   experimental: {
     externalDir: true,
   },
-};
-
-module.exports = withPWA(nextConfig);
+});
